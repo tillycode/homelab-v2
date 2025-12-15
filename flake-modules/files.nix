9df8cff2f79188ffe57a5e89e8d7f7c7f3fcba12
Ziping Sun <me@szp.io>
2025-12-16 00:18:57 +0800

    add files flake-module

---
+++ flake-modules/files.nix
@@ -0,0 +1,96 @@
+{ flake-parts-lib, lib, ... }:
+let
+  inherit (flake-parts-lib) mkPerSystemOption;
+  inherit (lib)
+    mkOption
+    types
+    literalExpression
+    mkDefault
+    mkIf
+    ;
+  fileOption =
+    pkgs:
+    {
+      name,
+      config,
+      ...
+    }:
+    {
+      options = {
+        target = mkOption {
+          type = types.str;
+          defaultText = literalExpression "name";
+          description = "Path to the target file relative to base path";
+        };
+        source = mkOption {
+          type = types.path;
+          description = "Path of the source file or directory.";
+        };
+        text = mkOption {
+          type = types.nullOr types.lines;
+          default = null;
+          description = "Text of the file.";
+        };
+        executable = mkOption {
+          type = types.bool;
+          default = false;
+          description = "Whether the file should be executable.";
+        };
+      };
+      config = {
+        target = mkDefault name;
+        source = mkIf (config.text != null) (
+          mkDefault (
+            pkgs.writeTextFile {
+              name = name;
+              inherit (config) text executable;
+            }
+          )
+        );
+      };
+    };
+in
+{
+  options.perSystem = mkPerSystemOption (
+    { config, pkgs, ... }:
+    {
+      options.files = {
+        files = mkOption {
+          type = types.attrsOf (types.submodule (fileOption pkgs));
+          default = { };
+          description = "Files to link into the repository";
+        };
+        shellHook = mkOption {
+          type = types.lines;
+          internal = true;
+          readOnly = true;
+        };
+      };
+      config.files.shellHook = lib.optionalString (config.files.files != { }) ''
+        link_file() {
+            local target="$1"
+            local source="$2"
+            local current_source
+            current_source=$(readlink "$target")
+            if [[ $? -eq 0 && "$current_source" == "$source" ]]; then
+                return
+            fi
+            mkdir -p "$(dirname "$target")"
+            ln -sf "$source" "$target"
+            echo -e "\e[32mlink $target updated\e[0m"
+        }
+        ${lib.concatMapStrings (
+          f:
+          "link_file ${
+            lib.escapeShellArgs [
+              f.target
+              f.source
+            ]
+          }\n"
+        ) (lib.attrValues config.files.files)}
+        unset -f link_file
+      '';
+
+    }
+  );
+}
--- flake/devshell.nix
+++ flake/devshell.nix
@@ -4,6 +4,7 @@
     {
       devshells.default = {
         devshell.startup.pre-commit-hook.text = config.pre-commit.installationScript;
+        devshell.startup.files.text = config.files.shellHook;
         devshell.packages = with pkgs; [
           deploy-rs
           sops
